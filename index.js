a084bb1215ac4ac51d1b7808886fcf119d75ed15
MarineSniper666 <robert.lake2@snhu.edu>
2026-03-25 04:57:22 -0500

    Module 4 complete - fixed routing, restored full project, added 404 easter egg

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRouter);
 app.use('/', indexRouter);
 
-
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
